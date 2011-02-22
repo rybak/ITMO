b9a1df2952786ecbe9dc77555ed9f1af5659a0c8
rybak.a.v@gmail.com <rybak.a.v@gmail.com@26ace5ff-866b-dd10-8287-efe74049009f>
2011-02-22 22:41:19 +0000

    New problem set structure

--- problem1.1/problem1.1.cpp
+++ problem1.1/problem1.1.cpp
@@ -1,17 +1,16 @@
 #include <fstream>
 
-// spiral
-
 int main()
 {
    std::ifstream input("in.txt");
    std::ofstream output("out.txt");
 
    int n = 0, i = 1, x = 0, y = 0, dx = 1, dy = 0;
-   bool c;
-
+   
    input >> n;
-   output << x << ' ' << y << '\n';
+
+   if (n > 0)
+		output << x << ' ' << y << '\n';
 
    while (i < n)
    {
@@ -19,30 +18,29 @@ int main()
       x = x + dx;
       y = y + dy;
       output << x << ' ' << y << '\n';
-      c = false;
-      if (!c && (dx > 0))
+      if (dx > 0)
       {
          dy = dx;
          dx = 0;
-         c = true;
+         continue;
       }
-      if (!c && (dy > 0))
+      if (dy > 0)
       {
          dx = -dy - 1;
          dy = 0;
-         c = true;
+			continue;
       }
-      if (!c && (dx < 0))
+      if (dx < 0)
       {
          dy = dx;
          dx = 0;
-         c = true;
+			continue;
       }
-      if (!c && (dy < 0))
+      if (dy < 0)
       {
          dx = - dy + 1;
          dy = 0;
-         c = true;
+         continue;
       }
    }
 
