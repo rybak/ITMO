2727ce6295f3afd673c6bd4f2df400d832bd0dfe
rybak.a.v@gmail.com <rybak.a.v@gmail.com@26ace5ff-866b-dd10-8287-efe74049009f>
2011-06-14 06:57:32 +0000

    4.1 4.2 blanks

---
+++ problem4.2/problem4.2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+
+   return 0;
+}
---
+++ problem4.2/vector.h
@@ -0,0 +1,12 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+template<int n, typename T>
+
+struct list
+{
+   const long num = n;
+   typedef T tail;
+};
+
+#endif
