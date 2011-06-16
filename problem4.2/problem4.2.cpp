5c7c1a1a2edfa8f642b3c0371ca54a733d6250df
rybak.a.v@gmail.com <rybak.a.v@gmail.com@26ace5ff-866b-dd10-8287-efe74049009f>
2011-06-16 13:14:34 +0000

    it seems it works

--- problem4.2/problem4.2.cpp
+++ problem4.2/problem4.2.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
+#include <fstream>
+#include "list.h"
+#include "sort.h"
 
-using std::cout;
+typedef
+   list
+   <
+      4,
+      list
+      <
+         3,
+         list
+         <
+            2,
+            list
+            <
+               1,
+               empty_list
+            >
+         >
+      >
+   > List;
+typedef merge_sort<List>::sorted_list Sorted_List;
 
 int main()
 {
-
+   std::ofstream output("out.txt");
+   print_list<List>(output);
+   output << "\n";
+   print_list<Sorted_List>(output);
    return 0;
 }
--- problem4.2/sort.h
+++ problem4.2/sort.h
@@ -1,8 +1,94 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include "vector.h"
+#include "list.h"
+#include "struct_if.h"
+/******************************************************************************/
+/* struct merge output merged_list */
 
+template<typename List1, typename List2>
+struct merge
+{
+   typedef
+      typename struct_if
+      <
+         (List1::value) < (List2::value),
+         typename list
+         <
+            List1::value,
+            typename merge
+            <
+               typename List1::tail,
+               List2
+            >::merged_list
+         >,
+         typename list
+         <
+            List2::value,
+            typename merge
+            <
+               List1,
+               typename List2::tail
+            >::merged_list
+         >
+      >::result merged_list;
+};
+
+template<typename List1>
+struct merge<List1, empty_list>
+{
+   typedef List1 merged_list;
+};
+
+template<typename List2>
+struct merge<empty_list, List2>
+{
+   typedef List2 merged_list;
+};
+
+/******************************************************************************/
+/* struct merge_sort output sorted_list */
+
+template<typename List>
+struct merge_sort
+{
+   static const int length = list_length<List>::value;
+   typedef
+      typename merge
+      <
+         typename merge_sort
+         <
+            typename list_partition_first
+            <
+               List,
+               length / 2
+            >::list_part
+         >::sorted_list,
+         typename merge_sort
+         <
+            typename list_partition_second
+            <
+               List,
+               length / 2
+            >::list_part
+         >::sorted_list
+      >::merged_list sorted_list;
+};
+
+template<>
+struct merge_sort<empty_list>
+{
+   typedef empty_list sorted_list;
+};
+
+template<long value>
+struct merge_sort<list<value, empty_list> >
+{
+   typedef list<value, empty_list> sorted_list;
+
+};
+
+/******************************************************************************/
 
 
 #endif
