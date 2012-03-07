f2d58860ea5dec75e4db87be0644b2a87d685893
Andrei Rybak <rybak.a.v@gmail.com>
2012-03-08 02:40:36 +0400

    checker code, need method to say about PE

--- cg2012.1/checkers/04_point_in_general_polygon/checker.cpp
+++ cg2012.1/checkers/04_point_in_general_polygon/checker.cpp
@@ -4,9 +4,11 @@
 #include <iterator>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_2_algorithms.h>
+#include <CGAL/enum.h>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel::Point_2 Point_2;
-
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point;
 
 int main(int argc, char* argv[])
 {
@@ -19,57 +21,81 @@ int main(int argc, char* argv[])
     ifstream input(argv[1]);
     double x, y;
     input >> x >> y;
-    Point_2 point(x, y); // point to check
+    Point point(x, y); // point to check
     
     size_t n; // amount of vertexes in polygon
     input >> n;
-    vector<Point_2> polygon(n);
+    vector<Point> polygon(n);
     for (size_t i = 0; i < n; ++i)
     {
         double x, y;
         input >> x >> y;
-        polygon[i] = Point_2(x, y);
+        polygon[i] = Point(x, y);
     }
 
     size_t m;
     input >> m;
-    vector<size_t> count(m); // amount of vertexes in hole[i]
-    vector<vector<Point_2> > holes(m);
+    vector<vector<Point> > holes(m);
     for (size_t i = 0; i < m; ++i)
     {
-        input >> count[i];
-        holes[i] = vector<Point_2>(count[i]);
-        for (size_t j = 0; j < count[i]; ++j)
+        size_t count;
+        input >> count;
+        holes[i] = vector<Point>(count);
+        for (size_t j = 0; j < count; ++j)
         {
             double x, y;
             input >> x >> y;
-            holes[i][j] = Point_2(x, y);
+            holes[i][j] = Point(x, y);
         }
     }
     input.close();
 
-    bool right_answer = false;
+    int right_answer = 0;
 
-    // TODO solution here
+    switch (CGAL::bounded_side_2(polygon.begin(), polygon.end(), point, K()))
+    {
+    case CGAL::ON_BOUNDED_SIDE:
+        right_answer = 1; // inside polygon, but maybe in one of holes
+        break;
+    case CGAL::ON_BOUNDARY:
+        right_answer = 0; // on boundary of polygon
+        break;
+    case CGAL::ON_UNBOUNDED_SIDE:
+        right_answer = -1; // outside of polygon
+        break;
+    }
+
+    for (size_t i = 0; (i < m) && (right_answer == 1); ++i)
+    {
+        switch (CGAL::bounded_side_2(holes[i].begin(), holes[i].end(), point, K()))
+        {
+        case CGAL::ON_BOUNDED_SIDE: // inside holes[i]
+            right_answer = -1;
+            break;
+        case CGAL::ON_BOUNDARY: // on boundary of holes[i]
+            right_answer = 0;
+            break;
+        case CGAL::ON_UNBOUNDED_SIDE: // outside of holes[i]
+            break;
+        }
+    }
 
     ifstream output(argv[2]);
-    std::string s;
-    output >> s;
+    //std::string s;
+    int answer;
+    output >> answer;
     output.close();
-    bool answer;
-    if (s == "YES" || s == "NO")
+    
+    if ((answer == -1) || (answer == 1) || (answer == 0))
     {
-        answer = (s == "YES");
+        if (answer == right_answer)
+        {
+            return 0; // AC
+        }
+        return 1; // WA   
     }
     else
     {
         // PE
-    }
-
-    if (answer == right_answer)
-    {
-        return 0; // AC
-    }
-    
-    return 1; // WA
+    }    
 }
