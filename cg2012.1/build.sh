cd2b6fdbbcd07899e70d6bb532df4178856955ab
Alexey Sergushichev <alsergbox@gmail.com>
2012-03-25 18:12:20 +0400

    reorganised a little

---
+++ cg2012.1/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+ok="01 04 09"
+
+cmake_build() {
+    mkdir bin || true
+    cd bin
+    cmake ..
+    cmake --build .
+    cd - 
+}
+
+DST_DIR=`pwd`
+SRC_DIR=`pwd`/../src/cg2012.1/
+
+for p in ${ok}
+do
+    echo $p
+    cd $SRC_DIR/checkers/${p}*
+    bin=`pwd`/bin
+    cmake_build
+    mkdir $DST_DIR/$p || true
+
+    pushd $DST_DIR/$p
+    cp $bin/*check* check
+    popd
+
+    cd ../../statements/${p}*
+    cp statement.pdf $DST_DIR/$p
+
+done
+
