c6c483f64da30a9ff3f09d2792d383e3e1f2dccd
Andrey Vasin <5836640+rabodaber@users.noreply.github.com>
2012-03-31 22:51:53 +0400

    accuracy OVER 9000

--- cg2012.1/checkers/09-polyline_simplification_no_topology_preservation/checker.cpp
+++ cg2012.1/checkers/09-polyline_simplification_no_topology_preservation/checker.cpp
@@ -3,12 +3,16 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+#include <CGAL\Gmpq.h>
+
+typedef CGAL::Gmpq gmp;
+
 
 struct Point
 {
-	double x, y;
+	gmp x, y;
 
-	Point (double _x = 0, double _y = 0):x(_x),y(_y){};
+	Point (gmp _x = 0, gmp _y = 0):x(_x),y(_y){};
 
 	Point operator-(const Point& a) const
 	{
@@ -24,39 +28,46 @@ struct Point
 struct Segment
 {
 	Point a, b;
-
-	Segment (double _ax = 0, double _ay = 0, double _bx = 0, double _by = 0):a(_ax, _ay),b(_bx, _by){};
+	Segment (gmp _ax = 0, gmp _ay = 0, gmp _bx = 0, gmp _by = 0):a(_ax, _ay),b(_bx, _by){};
 	Segment (Point _a, Point _b):a(_a),b(_b){};
 };
 
-double distancePointPoint(const Point& a, const Point& b)
+gmp pow(gmp x, int z)
 {
-	return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+	gmp ans = 1;
+	for (int i = 0; i < z; ++i)
+		ans *= x;
+	return ans;
+}
+
+gmp distancePointPoint(const Point& a, const Point& b)
+{
+	return (pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
 // Определяем попадает ли перпендикуляр к прямой через точку непосредственно в отрезок и в зависимости от
 // этого находим расстояние от точки до прямой или от точки до концов отрезка и берем минимум
-double distancePointSegment(const Point& point, const Segment& segment)
+gmp distancePointSegment(const Point& point, const Segment& segment)
 {	
-	double r1 = distancePointPoint(point, segment.a);
-	double r2 = distancePointPoint(point, segment.b);
-	double r12 = distancePointPoint(segment.a, segment.b);
+	gmp r1 = distancePointPoint(point, segment.a);
+	gmp r2 = distancePointPoint(point, segment.b);
+	gmp r12 = distancePointPoint(segment.a, segment.b);
 
 	if ( r1 >= distancePointPoint(Point(r2,r12), Point(0,0)) || r2 >= distancePointPoint(Point(r1,r12), Point(0,0)))
 		return std::min(distancePointPoint(point, segment.a),distancePointPoint(point, segment.b));
 
-	return abs((segment.a.y - segment.b.y) * point.x + (segment.b.x - segment.a.x) * point.y + (segment.a.x * segment.b.y - segment.b.x * segment.a.y)) / sqrt(pow(segment.b.x - segment.a.x,2)+pow(segment.b.y - segment.a.y,2));
+	return pow((segment.a.y - segment.b.y) * point.x + (segment.b.x - segment.a.x) * point.y + (segment.a.x * segment.b.y - segment.b.x * segment.a.y), 2) / (pow(segment.b.x - segment.a.x,2) + pow(segment.b.y - segment.a.y,2));
 }
 
-void DouglasPeucker(const int& begin, const int& end, const std::vector<Point>& points, std::vector<bool>& usage, const double& eps)
+void DouglasPeucker(const int& begin, const int& end, const std::vector<Point>& points, std::vector<bool>& usage, const gmp& eps)
 {
-	double max = -1;
+	gmp max = -1;
 	int num = -1;
 	Segment segment = Segment(points[begin], points[end]);
 
 	for (int i = begin + 1; i < end; i++)
 	{
-		double dist = distancePointSegment(points[i], segment);
+		gmp dist = distancePointSegment(points[i], segment);
 		if (max < dist && dist > eps)
 		{
 			max = dist;
@@ -72,12 +83,12 @@ void DouglasPeucker(const int& begin, const int& end, const std::vector<Point>& 
 	}
 }
 
-bool checkOnWrongPoints(const int& begin, const int& end, const std::vector<Point>& points, const double& eps)
+bool checkOnWrongPoints(const int& begin, const int& end, const std::vector<Point>& points, const gmp& eps)
 {
 	Segment segment = Segment(points[begin], points[end]);
 
 	for(int i = begin + 1; i < end; i++)
-		if (distancePointSegment(points[i], segment) > eps)
+		if ( distancePointSegment(points[i], segment) > eps)
 			return true;
 
 	return false;
@@ -97,11 +108,11 @@ int main(int argc, char* argv[])
 	std::vector<Point> points(n);
 	for (int i = 0; i < n; i++)
 	{
-		double x, y;
+		gmp x, y;
 		in >> x >> y;
 		points[i] = Point(x, y);
 	}
-	double eps;
+	gmp eps;
 	in >> eps;
 	in.close();
 
