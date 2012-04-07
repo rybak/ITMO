a5e61d7aafbe5357c8e8faccab3d3de8c58096a2
rybak.a.v@gmail.com <rybak.a.v@gmail.com@26ace5ff-866b-dd10-8287-efe74049009f>
2012-04-07 09:47:14 +0000

    new input/output files

--- contest2012.1/01/main.cpp
+++ contest2012.1/01/main.cpp
@@ -108,8 +108,8 @@ int main()
     using std::cin;
     using std::cout;
     using std::vector;
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
     
     size_t n;
     cin >> n;
