9e0208c3c21c70c377b3f8c024373aed9021d07b
rybak.a.v@gmail.com <rybak.a.v@gmail.com@26ace5ff-866b-dd10-8287-efe74049009f>
2012-04-11 15:32:57 +0000

    tabs to spaces in main.cpp

--- cg2012.1/01/main.cpp
+++ cg2012.1/01/main.cpp
@@ -105,7 +105,7 @@ void quick_hull(const std::vector<point> &points, std::vector<size_t> &hull)
 
 int main()
 {
-	const int magic = 42;
+    const int magic = 42;
     using std::cin;
     using std::cout;
     using std::vector;
@@ -126,10 +126,10 @@ int main()
 
     size_t m = hull.size();
     cout << m << '\n';
-	cout.setf(std::ios::fixed);
-	cout.precision(magic);
+    cout.setf(std::ios::fixed);
+    cout.precision(magic);
     for (size_t i = 0; i < m; ++i)
     {
-		cout << points[hull[i]].x << ' ' << points[hull[i]].y << '\n';
+        cout << points[hull[i]].x << ' ' << points[hull[i]].y << '\n';
     }
 }
