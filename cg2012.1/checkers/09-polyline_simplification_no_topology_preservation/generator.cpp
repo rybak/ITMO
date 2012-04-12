c34d1b38363ed18dde1e6eed84f32804b3984cd7
Andrey Vasin <5836640+rabodaber@users.noreply.github.com>
2012-04-12 19:39:49 +0400

    generator added

---
+++ cg2012.1/checkers/09-polyline_simplification_no_topology_preservation/generator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stdio.h>
+#include "math.h"
+#include <ctime>
+#include <random>
+#include <iostream>
+
+double random(){
+	double a = (double)((abs(rand())) % 10000)/10000;
+	return a;
+}
+
+int main(){
+	std::srand(time(0));
+	std::ofstream out("performance_test.txt");
+	out << 1000000 << "\n";
+	for (int i = 0; i < 1000000; ++i){
+		out << random() << " " << random() << "\n";
+	}
+	out << random() / 10;
+}
