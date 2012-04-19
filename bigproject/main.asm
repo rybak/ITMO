a74e90743fde1c13b82814f9d3260ef3284215f0
Andrei Rybak <rybak.a.v@gmail.com>
2012-04-20 01:21:52 +0400

    big project new functions, etc
    * float vars ballx, bally
    * pady in .rdata
    * vars vx, vy for speed - not used yet
    * ballxint, ballyint for draw_pad function

--- bigproject/main.asm
+++ bigproject/main.asm
@@ -2,9 +2,12 @@ extern	__imp__SetConsoleTextAttribute@8
 extern	__imp__GetStdHandle@4
 extern	__imp__WriteConsoleA@20
 extern	__imp__ExitProcess@4
+extern	__imp__Sleep@4
+
 extern	__imp__wsprintfA
 extern 	__imp__printf
 extern 	__imp__system
+extern	__imp___getch
 %assign MB_ICONINFORMATION 40h
 global _main
 
@@ -14,23 +17,32 @@ section .rdata
 attr dd           0000000011001001b
 
 cwidth dd 79
+_cwidth dd 81
 cheight dd 25
 
 pausestr db "pause",0
 intformat db " %d ",0
 
 section .bss
-stdin: resb 4
-stdout: resb 4
-written: resb 4
-doublebuf: resb 8
+stdin: resd 1
+stdout: resd 1
+written: resd 1
+doublebuf: resq 1
 buffer: resb 2000 ; = 80 * 25
 
 section .data
 padcoord dd 39
 padhwidth dd 5
-ballx dd 10
-bally dd 10
+
+ballx dd 10.0
+bally dd 10.0
+; speed
+vx dd +1.0
+vy dd -1.0
+
+section .bss
+ballxint: resd 1
+ballyint: resd 1
 
 section .text
 
@@ -42,15 +54,38 @@ _main:
 	push dword [cheight]
 	call print_int
 	call println
-
 	call clean_buffer
+
 	call draw_pad
 	call draw_ball
-	call print_buffer
-	
+
+	main_loop:
+		call println
+		call print_buffer
+		call sleep
+		;cmp eax, 'h'
+		mov eax, 1
+		cmp eax, 0
+;		jnz main_loop
+
+	_getch_testing:
+		call [__imp___getch]
+		push eax
+		push dword 5
+		push dword 15
+		call draw_char_xy
+		call print_buffer
+
 exit:
 	push 0
 	call [__imp__ExitProcess@4]
+
+sleep:
+	pushad	
+		push dword 30
+		call [__imp__Sleep@4]
+	popad
+	ret
 
 draw_pad:
 	pushad
@@ -76,7 +111,7 @@ draw_pad:
 			push dword '='
 			push edi
 			push ecx
-			call draw_charxy
+			call draw_char_xy
 			inc ecx
 			cmp ecx, ebp
 			jnz draw_pad_loop
@@ -85,21 +120,27 @@ draw_pad:
 
 draw_ball:
 	pushad
-		mov ecx, [ballx]
-		mov edi, [bally]
+		fld dword[ballx]
+		frndint
+		fistp dword[ballxint]
+		fld dword[bally]
+		frndint
+		fistp dword[ballyint]
+		mov ecx, [ballxint]
+		mov edi, [ballyint]
 		push dword 'O'
 		push edi
 		push ecx
-		call draw_charxy
+		call draw_char_xy
 	popad
 	ret
 
-draw_charxy: ; (int x, int y, char c)
+draw_char_xy: ; (int x, int y, char c)
 	pushad
 		mov ecx, [esp + 20h + 4]
 		mov eax, [esp + 20h + 8]
 		mov ebx, [esp + 20h + 0Ch]
-		mov ebp, [cwidth]
+		mov ebp, [_cwidth]
 		mul ebp
 		add eax, ecx
 		mov byte[buffer + eax], bl
@@ -108,26 +149,40 @@ draw_charxy: ; (int x, int y, char c)
 
 print_buffer:
 	pushad
+		call println
+		call println
 		xor ecx, ecx
 		mov ebp, [cheight]
-		print_buffer_loop:
-			push ecx
-			call print_buffer_line
-			inc ecx
-			cmp ecx, ebp
-			jnz print_buffer_loop
+;		print_buffer_loop:
+;			push ecx
+;			call print_buffer_line
+;			inc ecx
+;			cmp ecx, ebp
+;			jnz print_buffer_loop
+		mov eax, [cheight]
+		mov ebx, [_cwidth]
+		mul ebx
+		mov ecx, buffer
+
+		push dword 0
+		push written
+		push eax
+		push ecx
+		push dword[stdout]
+		call [__imp__WriteConsoleA@20]
+
 	popad
 	ret
 
 print_buffer_line:
 	pushad ; esp OFFSET
 		mov eax, [esp + 20h + 4]
-		mov ebx, [cwidth]
+		mov ebx, [_cwidth]
 		mul ebx
 		add eax, buffer
 		push dword 0
 		push written
-		push dword [cwidth]
+		push dword [_cwidth]
 		push eax
 		push dword[stdout]
 		call [__imp__WriteConsoleA@20]
@@ -137,8 +192,33 @@ print_buffer_line:
 
 clean_buffer:
 	pushad
-	    xor ecx, ecx
+		mov eax, [cheight]
+		mov ebx, [_cwidth]
+		mul ebx
+		xor ecx, ecx
+		test_loop:
+			mov byte[buffer + ecx], '.'
+			inc ecx
+			cmp ecx, eax
+			jnz test_loop
+		mov edi, [cheight]
+		xor ecx, ecx
+		mov eax, buffer
 		mov ebx, [cwidth]
+		test_loop2:
+			add eax, ebx
+    		mov byte[buffer + eax], 13
+			inc eax
+			mov byte[buffer + eax], 10
+			inc eax
+			inc ecx
+			cmp ecx, edi
+			jnz test_loop2
+	popad
+	ret
+	    xor ecx, ecx
+		mov ebx, [_cwidth]
+		mov edi, [cwidth]
 		mov ebp, [cheight]
 		clean_loop_lines:
 			xor esi, esi
@@ -148,8 +228,14 @@ clean_buffer:
 				add eax, esi
 				mov byte[buffer + eax], '.'
 				inc esi
-				cmp esi, ebx
+				cmp esi, edi
 				jnz clean_loop_col
+			; adding \r
+			;sub eax, 2
+			mov byte[buffer + eax], 13
+			inc eax
+			mov byte[buffer + eax], 10
+			; /
 			inc ecx
 			cmp ecx, ebp
 			jnz clean_loop_lines
