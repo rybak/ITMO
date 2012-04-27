f14d79a1c67dc10131fad71d7213d673daebfa1f
Andrei Rybak <rybak.a.v@gmail.com>
2012-04-27 14:35:35 +0400

    initial commit for lesson 6

--- make.bat
+++ make.bat
@@ -3,7 +3,6 @@ bin\yasm -f win32 %1.asm && echo compiled
 IF ERRORLEVEL 1 goto fail
 rem link.exe /subsystem:console /entry:main %1.obj /libpath:"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Lib" /defaultlib:Kernel32.lib /defaultlib:user32.lib  && echo linked
 gcc %1.obj -m32 -o %1.exe
-rem %CD%\%1.exe
 
 IF ERRORLEVEL 1 goto fail
 goto end
