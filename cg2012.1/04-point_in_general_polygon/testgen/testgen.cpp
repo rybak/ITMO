1d461562417156f1f72f3f4d521dae41ff331e80
Andrei Rybak <rybak.a.v@gmail.com>
2012-04-29 17:59:40 +0400

    adaptive test

--- cg2012.1/04-point_in_general_polygon/testgen/testgen.cpp
+++ cg2012.1/04-point_in_general_polygon/testgen/testgen.cpp
@@ -13,14 +13,16 @@
 using std::vector;
 using std::bind;
 using std::string;
+using std::uniform_int_distribution;
 using std::uniform_real_distribution;
 using std::mt19937;
 using namespace std::placeholders;
 
 typedef uniform_real_distribution<double> double_distr;
-	
+typedef uniform_int_distribution<int> int_distr;
+
 const int seed = 1993;
-const int precision = 50;
+const int precision = 100;
 
 vector<test_case> get_hard_coded_tests()
 {
@@ -48,20 +50,19 @@ vector<test_case> get_hard_coded_tests()
 }
 
 const double pi = atan(1.0) * 4;
-const double min_coord = 10;
-const vector<double> max_coords({100.0, 10.0e4, 10.0e10});
+const double min_coord = 10.0;
+const vector<double> max_coords({100.0, 1.0e4, 1.0e10});
 	
 vector<test_case> generate_triangle_tests()
 {
 	vector<test_case> tests;
 	mt19937 engine(seed);
-	
 	Point A(0.0, 0.0);
 	for (size_t i = 0; i < max_coords.size(); ++i)
 	{
-		double_distr point_distr(0, max_coords[i]);
+		double_distr coord_distr(0, max_coords[i]);
 		double_distr triangle_distr(min_coord + 1, max_coords[i]);
-		auto point_gen = bind(point_distr, engine);
+		auto point_gen = bind(coord_distr, engine);
 		auto triangle_gen = bind(triangle_distr, engine);
 		for (size_t j = 0, m = 3; j < m; ++j)
 		{
@@ -81,18 +82,21 @@ vector<test_case> generate_triangle_tests()
 vector<test_case> generate_star_tests()
 {
 	const size_t size = 4;
-	vector<test_case> tests;
-	
-	mt19937 engine(seed ^ 0x91BAC);
 	const double tiny = 1e-2;
 	const double angle_period = pi / size;
 	double_distr angle_distr(tiny, angle_period / 2 - tiny);
 	
-	double_distr factor_distr(0.3, 0.7);
+	const double factor_min = 0.3;
+	const double factor_max = 0.7;
 	
+	double_distr factor_distr(factor_min, factor_max);
+	
+	mt19937 engine(seed ^ 0x91BAC);
+	const double radius_min_factor = 0.8;
+	vector<test_case> tests;
 	for (size_t i = 0; i < max_coords.size(); ++i)
 	{
-		double_distr radius_distr(0.8 * max_coords[i], max_coords[i]);
+		double_distr radius_distr(radius_min_factor * max_coords[i], max_coords[i]);
 		vector<Point> star;
 		for (size_t j = 0; j < 2 * size; ++j)
 		{
@@ -116,14 +120,60 @@ vector<test_case> generate_star_tests()
 	return tests;
 }
 
+vector<test_case> generate_adaptive_tests()
+{
+	vector<test_case> tests;
+	double small_eps = 0.5 * std::numeric_limits<double>::epsilon();
+	
+	const double small_base = 1.0;
+	while (((small_base + small_eps) == small_base)
+		|| ((small_base - small_eps) == small_base))
+	{
+		small_eps *= 2;
+	}
+	double_distr coord_distr(small_base - small_eps, small_base + small_eps);
+	mt19937 engine(seed ^ 0xFE11A5);
+	auto coord_gen = bind(coord_distr, engine);
+	
+	volatile const double big_base = 10e10;
+	volatile double big_eps = 1.0e-5;
+	while (((big_base + big_eps) == big_base)
+		|| ((big_base - big_eps) == big_base))
+	{
+		big_eps *= 2;
+	}
+	vector<Point> polygon(
+	{// diamond along line y = x
+		Point(1, 1),
+		Point(big_base, big_base - big_eps),
+		Point(big_base, big_base),
+		Point(big_base - big_eps, big_base)
+	});
+	
+	const size_t tests_count = 10;
+	for (size_t i = 0; i < tests_count; ++i)
+	{
+		Point q(coord_gen(), coord_gen());
+		tests.push_back({q, polygon, {/*no holes*/} });
+	}
+	return tests;
+}
 
 vector<test_case> generate_correctness_tests()
 {
-	vector<test_case> tests(get_hard_coded_tests());
-	vector<test_case> triangle_tests(generate_triangle_tests());
-	tests.insert(tests.end(), triangle_tests.begin(), triangle_tests.end());
-	vector<test_case> star_tests(generate_star_tests());
-	tests.insert(tests.end(), star_tests.begin(), star_tests.end());
+	vector<vector<test_case> > test_suit(
+	{
+		get_hard_coded_tests(),
+		generate_triangle_tests(),
+		generate_star_tests(),
+		generate_adaptive_tests()
+	});
+	vector<test_case> tests;
+	for (auto it = test_suit.begin(); it != test_suit.end(); ++it)
+	{
+		tests.insert(tests.end(), (*it).begin(), (*it).end());
+	}
+	
 	return tests;
 }
 
