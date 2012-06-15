30b3ff4f0452ff00c0c5dcf29723df8c5fff902d
Andrew Shulayev <ddrone@users.noreply.github.com>
2012-06-16 01:04:07 +0400

    statement generator seems to be working, now testing

--- cg2012.1/generate.py
+++ cg2012.1/generate.py
@@ -4,6 +4,12 @@ tasks = []
 
 for directory, subdirs, files in os.walk('.'):
   if directory.endswith('statement'):
-    tasks.append(directory.split('/')[1])
+    candidates = filter(lambda name: name.endswith('tex') and not name.startswith('statement'), files)
+    if len(candidates) == 1:
+      tasks.append((directory.split('/')[1], candidates[0]))
 
-print tasks
+tasks.sort()
+
+f = open('tasks.tex', 'w')
+f.writelines(map(lambda (dirname, filename): '\\input %s/statement/%s\n' % (dirname, filename), tasks))
+f.close()
