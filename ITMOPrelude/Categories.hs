0f4661e8b8baa67d97f870225a69a8967901e508
andreyrybak <andreyrybak@localhost>
2012-10-26 13:30:41 +0400

    Monad State initial commit

--- ITMOPrelude/Categories.hs
+++ ITMOPrelude/Categories.hs
@@ -51,30 +51,11 @@ instance Monad (Either a) where
     (Left a) >>= f = Left a
     (Right a) >>= f = f a
 
--- class (Monad m) => (MonadFish' m) where
---     (>=>) :: (a -> m b) -> (b -> m c) -> a -> m c
---     (>=>) f g a = (return a) >>= f >>= g
--- 
--- class (Monad m) => (MonadJoinFmap m) where
---     monadfmap :: (a -> b) -> m a -> m b
---     monadfmap f m = m >>= (return . f)
---     join' :: m (m a) -> m a
---     join' = flip (>>=) id
--- 
--- class (MonadFish' m) => (Monad2 m) where
---     bind2 :: m a -> (a -> m b) -> m b
---     bind2 m f = (>=>) id f m
--- 
--- class (MonadFish' m) => (MonadjoinFmap2 m) where
---     monadfmap2 :: (a -> b) -> m a -> m b
---     monadfmap2 f = id >=> (return . f)
---     join'2 :: m (m a) -> m a
---     join'2 = id >=> id
--- 
--- class (MonadJoinFmap m) => (MonadFish'2 m) where
---     fish :: (a -> m b) -> (b -> m c) -> a -> m c
---     fish f g a = join' $ monadfmap g $ f a
--- 
--- class (MonadJoinFmap m) => (Monad3 m) where
---     bind3 :: m a -> (a -> m b) -> m b
---     bind3 m f = join' $ monadfmap f m
+--------------------------------------------------------------------------------
+-- Монада State
+
+newtype State s a = State { runState :: s -> (s, a) }
+
+instance Monad (State s) where
+    return = ?
+    (>>=) = ?
