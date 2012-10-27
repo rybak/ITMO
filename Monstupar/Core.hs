8947e82c49ef7b0ad1de64c156c580c47d9d376f
andreyrybak <andreyrybak@localhost>
2012-10-27 19:42:53 +0400

    all "undefined" in Core.hs

--- Monstupar/Core.hs
+++ Monstupar/Core.hs
@@ -14,10 +14,15 @@ data ParseError = ParseError
                 deriving (Show) -- лишь бы show был
 
 newtype Monstupar s a = Monstupar { runParser :: [s] -> Either ParseError ([s], a) }
+-- runParser :: (Monstupar s a) -> [s] -> Either ParseError ([s], a)
 
 instance Monad (Monstupar s) where
     return a = Monstupar $ \s -> Right (s , a)
-    ma >>= f = undefined
+-- (>>=) ::
+-- (Monstupar s) a -> (a -> (Monstupar s) b) -> (Monstupar s) b
+    ma >>= f = Monstupar $ \s -> case (runParser ma s) of
+        Right (s', a) -> runParser (f a) s'
+        Left _        -> Left ParseError
 
 --------------------------------------------------------------------------------
 -- Примитивные парсеры.
@@ -43,12 +48,15 @@ infixr 2 <|>
 -- Сначала первый парсер, если он фейлится, то второй
 (<|>) :: Monstupar s a -> Monstupar s a -> Monstupar s a
 a <|> b = Monstupar $ \s -> case runParser a s of
-    Left c -> Left c
-    Right _ -> runParser b s
+    Left _  -> runParser b s
+    a -> a
 
 -- В голове ввода сейчас нечто, удовлетворяющее p
 like :: (s -> Bool) -> Monstupar s s
-like p = Monstupar $ \s -> undefined
+like p = Monstupar $ \s -> let a = head s in
+    if p $ head s
+        then Right (tail s, head s)
+        else Left ParseError
 
 -- Сюда можно добавлять ещё какие-то примитивные парсеры
 -- если они понадобятся
