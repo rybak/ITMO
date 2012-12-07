4970479d61fec3aff0b4e7736da32da4a7f5151c
Andrei Rybak <rybak.a.v@gmail.com>
2012-12-08 01:43:35 +0400

    rl_close

--- unsed/readlines.c
+++ unsed/readlines.c
@@ -15,10 +15,22 @@ RL* rl_open(int fd, size_t max_size) {
     if (rl->buf == NULL) {
         return NULL;
     }
+    rl->fd = fd;
     rl->max_size = max_size;
     return rl;
 }
 
 size_t rl_max_size(RL *rl) {
     return rl->max_size;
+}
+
+int rl_close(RL *rl) {
+    free(rl->buf);
+    int res = close(rl->fd);
+    free(rl);
+    return res;
+}
+
+int rl_readline(RL *rl, char *buf, size_t buf_size) {
+
 }
