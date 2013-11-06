161eff0bf604b671debcf89c28f6cca66485fb93
Andrei Rybak <rybak.a.v@gmail.com>
2013-11-07 00:35:03 +0400

    chat : receiver dummies

--- chat/receiver.cpp
+++ chat/receiver.cpp
@@ -7,8 +7,18 @@
 
 #include "receiver.h"
 
+{
+    struct chat_user
+    {
+        mac_addr_t ma;
+        string nick;
+
+
+    };
+}
 receiver::receiver()
 {
+    // init tcp listen sock
 }
 
 receiver::~receiver()
@@ -24,9 +34,16 @@ void receiver::cycle()
 
 void receiver::receive_messages()
 {
+    // accept TODO
+    char *buffer = new char[1024];
+    // read TODO
+    int cnt;
+    add_message(buffer, cnt);
+    delete[] buffer;
 }
 
 void receiver::print_messages()
 {
 
 }
+
