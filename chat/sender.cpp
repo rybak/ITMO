da630f374810b175d3daec35bed7856335533c96
Andrei Rybak <rybak.a.v@gmail.com>
2013-11-09 01:34:35 +0400

    chat : sender::send_message init connection

--- chat/sender.cpp
+++ chat/sender.cpp
@@ -2,10 +2,12 @@
 #include <cstring>
 #include <cstdio>
 
-#include "unistd.h"
+#include <unistd.h>
+#include <netinet/in.h>
 #include <ctime>
 
-#include "chat_message.h"
+// #include "chat_message.h"
+#include "common.h"
 #include "sender.h"
 
 sender::sender(uint16_t port)
@@ -21,7 +23,21 @@ sender::~sender()
 
 void sender::send_message(const user &u, const std::string &text)
 {
-    // connect to all computers
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        die("sender::send_message : socket");
+    }
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = port;
+    addr.sin_addr.s_addr = u.ip;
+    if (connect(sock, (sockaddr *) &addr, sizeof(addr)) < 0)
+    {
+        die("sender::send_message : connect");
+    }
+
+
 }
 
 
