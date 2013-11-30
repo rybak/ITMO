8efbbc49c397d96a1dad6e41dbaa2e2539576cf8
Andrey Rybak <rybak.a.v@gmail.com>
2013-11-30 15:12:38 +0400

    common.gp

--- lorenz/all.sh
+++ lorenz/all.sh
@@ -3,10 +3,12 @@
 if [ $# -eq 0 ];
 then RS="28.0"
 else RS=$@
+fi
 
 ./euler f $RS
 
-for i in $RS
-do
-    export NUM=$i; gnuplot -p oneE.gp
+for i in $RS; do
+    echo $i
+    gnuplot -e "NUM=$1" -p oneE.gp;
 done
+
--- lorenz/oneE.gp
+++ lorenz/oneE.gp
@@ -1,4 +1,6 @@
-n=$NUM
-title=sprintf("r = %.2f", n)
+load common.gp
+n=NUM
+set title sprintf("r = %.2f", n)
+set xlabel
 fn=sprintf("euler%.1f", n)
 splot fn
