cc6a7c7e22126992cff513bac9ed377e51c7fa30
Andrey Rybak <rybak.a.v@gmail.com>
2013-11-30 15:25:16 +0400

    verbose in all.sh

--- lorenz/all.sh
+++ lorenz/all.sh
@@ -5,17 +5,21 @@ then RS="28.0"
 else RS=$@
 fi
 
+echo "calculating $RS"
 ./euler f $RS
+echo "done"
 
 function call_gnuplot {
     r=$1
     m=$2
     gnuplot -e "NUM=$r" -p "one$m.gp"
 }
+echo "plotting"
 for i in $RS; do
-    echo $i
-    for m in "EE RK A IE"; do
+    echo "r = $i"
+    for m in EE RK A IE; do
         call_gnuplot $i $m
     done
 done
+echo "done"
 
