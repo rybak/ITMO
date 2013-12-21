879a9d90e56b43a19ccb2727fbcd46a54b4894cc
Andrei Rybak <rybak.a.v@gmail.com>
2013-12-21 04:56:44 +0400

    fs : test dir new cool Makefiles with includes

---
+++ common/common.mk
@@ -0,0 +1,14 @@
+# directories
+INCLUDE=../common
+CF=$(INCLUDE)/common
+
+# binaries
+CC=g++ -std=c++11 -O2 -I"$(INCLUDE)" -Wall
+CCO=$(CC) -c
+
+# libs
+LIB_CRYPTO=/usr/lib/libcrypto++.a
+LIB_OPENSSL=-lcrypto 
+
+# commands
+RM=rm -f
--- fs/main.cpp
+++ fs/main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
+using std::cout;
+using std::endl;
 
+#include "cryptopp/integer.h"
+#include "cryptopp/md5.h"
+using CryptoPP::Integer;
+#include <cryptopp/sha.h>
 
-int main()
-{
+// ~/lab/os-course/midterm/*.c <<< has shell-style sha calculation
 
+int main( int argc, char* argv[] ) {
+    CryptoPP::SHA256 sha256gen;
+    byte hashb[256];
+    const byte word[256] = "test";
+    sha256gen.CalculateDigest(hashb, word, 4 );
+    std::cout << (hashb[0] + 'A') << std::endl;
+    return 0;
 }
+
+//int main( int, char** )
+//{
+//    CryptoPP::MD5 hash;
+//    byte digest[ CryptoPP::MD5::DIGESTSIZE ];
+//    std::string message = "abcdefghijklmnopqrstuvwxyz";
+//
+//    hash.CalculateDigest( digest, message.c_str(), message.length() );
+//
+//    CryptoPP::HexEncoder encoder;
+//    std::string output;
+//    encoder.Attach( new CryptoPP::StringSink( output ) );
+//    encoder.Put( digest, sizeof(digest) );
+//    encoder.MessageEnd();
+//
+//    std::cout << output << std::endl;
+//
+//    Integer i;
+//    cout << "i: " << i << endl;
+//    return 0;
+//}
+//
+    //std::string password;
+    //std::getline(std::cin, password);
+
+
