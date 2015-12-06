97030cc1572f5fb54cd200b19c744992c7d1d065
Andrey Rybak <rybak.a.v@gmail.com>
2015-12-06 16:37:11 +0300

    move scopeCheck into separate module

--- rybak.andrey/src/Main.hs
+++ rybak.andrey/src/Main.hs
@@ -12,7 +12,6 @@ import LLanguage.BuiltIn
 parseProg = pParProgram . tokens
 
 typeCheck = undefined
-scopeCheck = undefined
 desugar = undefined
 codegen = undefined
 
---
+++ rybak.andrey/src/Scope.hs
@@ -0,0 +1,5 @@
+module Scope (
+    scopeCheck
+) where
+
+scopeCheck = undefined
