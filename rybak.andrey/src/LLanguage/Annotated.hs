44f606105de1b820b99df33de67fda4aa9523481
Andrey Rybak <rybak.a.v@gmail.com>
2016-02-13 14:45:24 +0300

    preparation for new typecheck code

--- rybak.andrey/src/LLanguage/Annotated.hs
+++ rybak.andrey/src/LLanguage/Annotated.hs
@@ -10,4 +10,7 @@ data ATopLevel a = ATopDecl ADecl | ATopFun String
 data ADecl = ADec PIdent ParLType
 	deriving (Eq, Ord, Show)
 
-
+data ABlock a = ABlockB [AStm a]
+	deriving (Eq, Ord, Show)
+data AStm a = ASDecl ADecl
+	deriving (Eq, Ord, Show)
