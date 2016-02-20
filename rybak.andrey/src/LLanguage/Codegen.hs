27c1a1accd0b50214e1a5f3c14a862667f11d5be
Andrey Rybak <rybak.a.v@gmail.com>
2016-02-20 19:30:48 +0300

    codegen of function pointer types
    +refactoring (e.g. compileName)
    +commented out unused definitions

--- rybak.andrey/src/LLanguage/Codegen.hs
+++ rybak.andrey/src/LLanguage/Codegen.hs
@@ -18,8 +18,10 @@ import Control.Monad.State hiding (void)
 import Control.Applicative
 import qualified Data.Map as M
 
-data Binding = Global String
-    deriving (Eq, Ord, Show, Read)
+-- data Binding = Global String
+--     deriving (Eq, Ord, Show, Read)
+
+isVarArgSupported = False
 
 data CodegenState = CodegenState {
     counter :: Int
@@ -50,32 +52,40 @@ compileProgram (AProg topLevels) = do
             LLVM.moduleDefinitions = varDefs ++ funDefs
         }
 
-getVars ts = [(pIdentToString pi, t) | ATopDecl (ADec pi t) <- ts]
+getVars ts = [(pi, t) | ATopDecl (ADec pi t) <- ts]
 getFuns ts = filter isATopFun ts
 
 convertToLLVMType :: ParLType -> T.Type -- from LLVM.G.A.T
 convertToLLVMType TInt = T.i32
 convertToLLVMType TVoid = T.void
 
-compileGlobalVar :: (String, ParLType) -> G.Global
-compileGlobalVar (varName, parType) = G.globalVariableDefaults {
-    G.name = LLVM.Name varName,
-    G.type' = convertToLLVMType parType
+compileGlobalVar :: (PIdent, ParLType) -> G.Global
+compileGlobalVar (pi, t) = G.globalVariableDefaults {
+    G.name = compileName pi,
+    G.type' = convertToLLVMType t
 }
+compileName pi = LLVM.Name $ pIdentToString pi
 
 codegenTopFun :: ATopLevel a -> Codegen G.Global
 codegenTopFun (ATopFun pi as rt b) = do
     ablocks <- codegenFunctionBody b
     return $ G.functionDefaults {
             G.returnType = compileType rt,
-            G.name = LLVM.Name $ pIdentToString pi,
-            G.parameters = compileParameters as,
+            G.name = compileName pi,
+            G.parameters = (compileParameters as, isVarArgSupported),
             G.basicBlocks = ablocks
         }
-codegenTopFun _ = error "Internal error : trying to codegen a function out of something else."
+codegenTopFun _ = error "Internal error: codegen function out of something else."
 
-compileType = const T.i32 -- TODO add ints and function pointers and stuff
-compileParameters = const ([], False) -- TODO add "([Parameter], Bool)
+compileType TVoid = T.void
+compileType TInt = T.i32
+compileType (TFun ats rt) = T.ptr $
+    T.FunctionType (compileType rt) (map compileType ats) isVarArgSupported
+compileParameters :: [ADecl] -> [G.Parameter]
+compileParameters = map compileParameter -- TODO add "([Parameter], Bool)
+compileParameter (ADec pi t) = G.Parameter (compileType t) (compileName pi) []
+
+
 codegenFunctionBody _ = return [] -- stub TODO add codegen
 
 
